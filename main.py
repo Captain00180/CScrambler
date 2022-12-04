64ca842c486945e8075eebcd61884e04a933352f
Filip Januška <f.januska@gmail.com>
2022-12-04 18:33:15 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,90 @@
+from random import randint, shuffle
+import argparse
+from pycparser import parse_file, c_generator, c_ast
+
+class Scrambler:
+    def __init__(self):
+        # Dictionary containing mappings of old names to new names
+        # key: old name
+        # value: new name
+        self.id_table = {}
+
+    def traverse(self, node):
+        if isinstance(node, (c_ast.Decl, c_ast.ID)):
+            node.name = self.scramble_name(node.name, node.coord.column, node.coord.line)
+        elif isinstance(node, c_ast.TypeDecl):
+            # Variable declaration
+            node.declname = self.scramble_name(node.declname, node.coord.column, node.coord.line)
+
+        elif isinstance(node, c_ast.Constant):
+            if node.type == 'string':
+                node.value = self.scramble_string(node.value)
+            if node.type == 'int':
+                node.value = self.scramble_int(node.value)
+
+        for child in node:
+            self.traverse(child)
+
+    def scramble_name(self, name, x, y):
+        if name == 'main':
+            return name
+        if name in self.id_table:
+            # Name has already been remapped, reuse it from the id_table
+            return self.id_table[name]
+
+        # Name hasn't been mapped yet, map it now
+        new_name = list(f'{"O"*x}{"0"*y}')
+        shuffle(new_name)
+        new_name = 'O' + ''.join(new_name)
+        self.id_table[name] = new_name
+        return new_name
+
+    def scramble_string(self, string):
+        # Transforms the string to a sequence of escaped hex bytes
+        res = ''
+        for char in string:
+            if char == '"':
+                res += char
+                continue
+            res += r'\x' + f'{hex(ord(char))[2:]}'
+        return res
+
+    def scramble_int(self, num):
+
+        def xor_obfuscate(x, iterations):
+            if iterations <= 0:
+                return x, 0
+            y = randint(1, 50000)
+            z = x ^ y
+            return z, *xor_obfuscate(y, iterations - 1)
+
+        # Converts the int literal to octal and adds multiple XOR obfuscations
+        num = int(num)
+        num_list = xor_obfuscate(num, 10)[:-1]
+        return '(' + '^'.join([oct(x).replace("o", "") for x in num_list]) + ')'
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        prog='CScrambler',
+        description='Simple C source code obfuscator.'
+    )
+
+    parser.add_argument('filename')
+
+    args = parser.parse_args()
+    print(args)
+
+
+    ast = parse_file('pycparser-master/examples/c_files/test.c')
+    ast.show(offset=2, attrnames=True)
+
+    #scrambler = Scrambler()
+    #scrambler.traverse(node=ast)
+
+    gen = c_generator.CGenerator()
+    output = gen.visit(ast)
+    output = output.replace('\n', '').replace('\r', '')
+    output = ' '.join(output.split())
+    print(output)
+        
